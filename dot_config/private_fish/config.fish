850724e9b752ddab999407bf065b8f9515bf462f
Pavlos Vinieratos <pvinis@gmail.com>
2025-02-27 16:56:09 +0200

    aha

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -29,6 +29,15 @@ alias oplogin='eval $(op signin --account my)'
 set fish_greeting
 fish_vi_key_bindings
 
+
+function c
+    if test -z "$argv"
+        code .
+    else
+        code $argv
+    end
+end
+
 function cc
     if test -z "$argv"
         cursor .
@@ -68,3 +77,7 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
