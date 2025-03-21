d245a539615ab6f0aebffedb7a7e20ff00091bf6
Pavlos Vinieratos <pvinis@gmail.com>
2025-03-21 11:41:00 +0200

    aha

--- dot_swiftbar/executable_top-things-item.1m.sh
+++ dot_swiftbar/executable_top-things-item.1m.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "⋅ $(osascript -e 'tell application "Things3" to get name of first to do of list "Today"') ⋅"
+echo "⋅ $(osascript -e 'tell application "Things3" to get name of first to do of list "Today" whose status is not completed') ⋅"
